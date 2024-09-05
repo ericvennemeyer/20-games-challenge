abe4f27ed552c63ad03faaa0e0e7fab7161583d4
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-09-05 10:47:12 -0700

    Create dict for player types, and getter and setter functions in GlobalPong. Make GlobalPong an autoload.

--- Pong/GlobalPong.gd
+++ Pong/GlobalPong.gd
@@ -1,7 +1,16 @@
 extends Node
 
-
+var players = {"p1": "human",
+			   "p2": "human"}
 
 
 func _ready() -> void:
 	pass # Replace with function body.
+
+
+func set_player_type(player: String, player_type: String) -> void:
+	players[player] = player_type
+
+
+func get_player_type(player: String):
+	return players[player]
