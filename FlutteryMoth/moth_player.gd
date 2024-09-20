3ccae19ada99eb460bd91a1ab1a2134b32d0f3d2
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-09-20 10:20:17 -0700

    Add floor and ceiling boundaries, and die() method in Moth script that is triggered on entering boundaries.

--- FlutteryMoth/moth_player.gd
+++ FlutteryMoth/moth_player.gd
@@ -1,6 +1,8 @@
+class_name Player
 extends CharacterBody2D
 
 const JUMP_VELOCITY = -400.0
+### TODO: Tweak jump velocity and gravity
 
 
 func _physics_process(delta: float) -> void:
@@ -13,3 +15,8 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
+
+
+func die() -> void:
+	print("The moth is dead.")
+	queue_free()
